ea4ff3374fc3c6b3e021774a8f63b9b2fd28e107
atheneares <132620116+atheneares@users.noreply.github.com>
2024-02-26 12:27:25 -0300

    Add files via upload

---
+++ Examen(dic)/codigo.ino
@@ -0,0 +1,81 @@
+/* Vcc --> 5V
+Trig -->  Pin digital 2
+        Echo --> Pin digital 3
+        GND  -->  GND
+
+   LED Rojo
+        Anodo (+) --> Resistor --> Pin digital 4
+
+   LED Amarillo
+        Anodo (+) --> Resistor --> Pin digital 5
+
+   LED Verde
+        Anodo (+) --> Resistor --> Pin digital 6
+
+   LED Azul
+        Anodo (+) --> Resistor --> Pin digital 7*/
+
+
+
+#define TRIG_PIN 2
+#define ECHO_PIN 3
+#define RED_LED_PIN 4
+#define YELLOW_LED_PIN 5
+#define GREEN_LED_PIN 6
+#define BLUE_LED_PIN 7
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(BLUE_LED_PIN, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  if (distance < 20) {
+    // Rojo
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(BLUE_LED_PIN, LOW);
+  } else if (distance < 30) {
+    // Amarillo
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(BLUE_LED_PIN, LOW);
+  } else if (distance < 40) {
+    // Azul
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(BLUE_LED_PIN, HIGH);
+  } else if (distance < 50) {
+    // Verde
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, HIGH);
+    digitalWrite(BLUE_LED_PIN, LOW);
+  } else {
+    // Apagar LEDs
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(BLUE_LED_PIN, LOW);
+  }
+}
+
+
